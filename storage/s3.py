b7fb51c2811d2d298386f4a675366e3aed7b5a53
Coolbit <neeoub@gmail.com>
2026-06-15 12:43:49 +0545

    Feat: Touchup watch feature

--- storage/s3.py
+++ storage/s3.py
@@ -3,7 +3,7 @@ from config import settings
 
 client = boto3.client("s3", endpoint_url=settings.s3_url)
 
-    
+
 def s3_transcoded_upload(file_path, key):
     try:
         client.upload_file(file_path, settings.bucket_name, key)
