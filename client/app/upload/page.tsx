90e317d24b0076b546ada8d2e389a27991ce6c63
Coolbit <neeoub@gmail.com>
2026-06-15 15:08:41 +0545

    Added ui

---
+++ client/app/upload/page.tsx
@@ -0,0 +1,15 @@
+"use client"
+
+import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
+import { Field } from "@/components/ui/field"
+import { useState, useEffect } from "react"
+
+export default function Upload() {
+    const [progress, SetProgress] = useState<string>("")
+    return (
+        <div className="flex flex-col flex-1 justify-center items-center bg-zinc-50 font-sans dark:bg-black">
+
+        </div>
+    )
+}
